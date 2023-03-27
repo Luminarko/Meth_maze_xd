c2d831a4818b71d992b4646a254d5d7aaffec14a
Imrich Zeleňák <92807683+Luminarko@users.noreply.github.com>
2023-03-27 13:44:47 +0200

    Add files via upload

--- meth_maze_xd/End.gd
+++ meth_maze_xd/End.gd
@@ -1,5 +1,7 @@
 extends Control
 
+func _ready():
+	$Button.grab_focus()
 
 func _on_Button_pressed():
 	get_tree().change_scene("res://Info.tscn")
--- meth_maze_xd/Info.gd
+++ meth_maze_xd/Info.gd
@@ -1,6 +1,7 @@
 extends Control
 
-
+func _ready():
+	$play.grab_focus()
 
 func _on_play_pressed():
 	get_tree().change_scene("res://Level_1.tscn")
